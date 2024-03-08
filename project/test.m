f2945b16ae4917e0c53d0bb186e49f81812d4545
Spencer Freeman <spencerfreeman1234@gmail.com>
2024-03-07 23:57:38 -0500

    add emag2 import function

---
+++ project/test.m
@@ -0,0 +1,7 @@
+clear;clc;close all
+
+
+ data = import_emag2_file("C:\Users\spenc\Desktop\school shit 2\Spring_2024\upper_atmosphere\project\data\EMAG2_V3_20170530\EMAG2_V3_20170530.csv", [1, Inf]);
+
+
+
