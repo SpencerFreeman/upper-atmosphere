5234f2d7348b495721c28fb42854ee799d9f726b
Spencer Freeman <spencerfreeman1234@gmail.com>
2024-03-09 16:54:41 -0500

    save EMAG2 data for blacksburg

--- project/test.m
+++ project/test.m
@@ -1,36 +1,61 @@
 clear;clc;close all
 
 
-data = import_emag2_file("C:\Users\spenc\Desktop\school shit 2\Spring_2024\upper_atmosphere\project\data\EMAG2_V3_20170530\EMAG2_V3_20170530.csv", [1, Inf]);
+raw_data = import_emag2_file("C:\Users\spenc\Desktop\school shit 2\Spring_2024\upper_atmosphere\project\data\EMAG2_V3_20170530\EMAG2_V3_20170530.csv", [1, Inf]);
 
 
 %%
 
-lat_range = [36 38];
-lon_range = [-81 -78];
+lat_range = [ 36.758719  37.556273];
+lon_range = [-80.874399 -79.464463];
 
-inds = data.LON > (360 + lon_range(1)) & data.LON < (360 + lon_range(2)) & ...
-    data.LAT > lat_range(1) & data.LAT < lat_range(2);
+inds = raw_data.LON > (360 + lon_range(1)) & raw_data.LON < (360 + lon_range(2)) & ...
+    raw_data.LAT > lat_range(1) & raw_data.LAT < lat_range(2);
 
-data_sub = data(inds, :);
+data = raw_data(inds, :);
+
+save('EMAG2_V3_Blacksburg-Roanoke', 'data')
 
 
-%%
+%% plotting
 clc; close all
 
 xq = linspace(360 + lon_range(1), 360 + lon_range(2), 75);
 yq = linspace(lat_range(1), lat_range(2), 70);
-x  = data_sub.LON;
-y  = data_sub.LAT;
-v  = data_sub.UpCont;
+x  = data.LON;
+y  = data.LAT;
+v  = data.UpCont;
 
 [Xq, Yq, Vq] = griddata(x, y, v, xq, yq', "linear");
 
-figure
-contourf(Xq, Yq, Vq)
+i = 200;
+data(i, :)
+V = data(i, :).UpCont
+Vi = griddata(x, y, v, data(i, :).LON, data(i, :).LAT, "linear")
+
+
+figure('WindowStyle', 'Docked')
+contourf(wrapTo180(Xq), Yq, Vq)
+
+hold on
+
+plot(data(i, :).LON-360,data(i, :).LAT,'*k')
+
+clr = [1 0 0];
+
+blacksburg_lla = [37.230139960377606, -80.4293122107635];
+plot(blacksburg_lla(2), blacksburg_lla(1), '*', 'Color',clr )
+text(blacksburg_lla(2), blacksburg_lla(1) + .1, 'Blacksburg', 'Color',clr)
+
+roanoke_lla = [37.2810435498791, -79.95796154021077];
+plot(roanoke_lla(2), roanoke_lla(1), '*', 'Color',clr)
+text(roanoke_lla(2), roanoke_lla(1) + .1, 'Roanoke', 'Color',clr)
+
 clim([min(v) max(v)])
-c = contourcbar;
+c = colorbar;
 c.Label.String = '(nT)';
+axis equal
+title('NOAA EMAG2 V3 Magnetic Anomaly')
 xlabel('Longitude (deg)')
 ylabel('Latitude (deg)')
 zlabel('Magnetic Anomaly (nT)')
