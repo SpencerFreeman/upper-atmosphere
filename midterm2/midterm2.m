0bbc3d393a2a0edc332832017d8a8d311fd5c932
Spencer Freeman <spencer.a.freeman2.civ@us.navy.mil>
2024-04-13 15:24:18 -0400

    finish midterm 2

--- midterm2/midterm2.m
+++ midterm2/midterm2.m
@@ -1,6 +1,19 @@
 clear;clc;close all
 
-
+% Solution Output:
+% Spencer Freeman, 4/12/2024
+% 
+% 2a: 4247.345825, m/s
+% 2b: 11669.486746, m/s
+% 2c: 103.039085, m
+% 2d: 0.055479, s
+% 2e: 0.104177, m/s
+% 2f: 5322.629358, nT
+% 
+% 3a: 201261.842171, ions/something...
+% 3b: 42.185130, 42.204214, 42.185249, ions/something... / s
+% 
+% 4: -3.781406e-12, Pa/km
 
 %% 2) A He+ ion is in near Earth space, at the geomagnetic equator. It has 
 % a mass of 6.64x10-27 kg, and a charge of +1.60x10-19. It has a kinetic 
@@ -8,7 +21,8 @@ clear;clc;close all
 % directed at an angle of 70 degrees away from the local geomagnetic field 
 % vector. The local geomagnetic field has a magnitude of 4700 nT, and a 
 % radius of curvature of 7300 km. Compute the following:
-fprintf('\n')
+fprintf('Solution Output:\n')
+fprintf('Spencer Freeman, 4/12/2024\n\n')
 
 r2cyc = 1/(2*pi); % cycles/radian
 
@@ -29,13 +43,13 @@ Rc_mag = 7300e3; % radius of curvature, m
 v_mag = sqrt(2*He_u/He_mass); % m/s
 v_B_para = v_mag * cos(dpsi); % m/s
 
-fprintf('2a: %e, m/s\n', v_B_para)
+fprintf('2a: %f, m/s\n', v_B_para)
 
 % b) The velocity of the particle perpendicular to the magnetic field, in m/s.
 
 v_B_perp = v_mag * sin(dpsi); % m/s
 
-fprintf('2b: %e, m/s\n', v_B_perp)
+fprintf('2b: %f, m/s\n', v_B_perp)
 
 % c) The gyroradius of the particle, in m.
 
@@ -125,16 +139,63 @@ fprintf('3b: %f, %f, %f, ions/something... / s\n', n_e, n_e_ode, n_e_tend)
 
 
 
+%% 4) You have obtained observations of the O+ density in the upper F-region 
+% near the magnetic pole. At this altitude, the ion temperature is 
+% approximately constant with height, and the electron temperature is 1.5x 
+% the ion temperature. 
 
+% Find the vertical gradient in the pressure associated with the O+ plasma 
+% in this region. You may ignore any other sources of pressure, and you may 
+% assume that O+ acts as an ideal gas when computing pressure (see Part 1, 
+% Lesson 1 slides if needed). 
+fprintf('\n')
+clear
 
+n_O = 17312751370; % O+ density, m^-3
 
+T_O = 1606; % Ion temperature, K
+T_e = T_O * 1.5; % electron temperature, K
+h = 584; % Altitude of observation, km
 
+Re = 6378; % Radius of Earth, km
+mass_O = 2.66e-26; % Mass of O+, kg
+G = 6.67e-11; % Gravitational constant G 
+mass_earth = 5.97e24; % Mass of Earth, kg
+kb = 1.38e-23; % Boltzmann's constant
 
+% use iterative method to calculate density
+km2m = 1000; % m/km
+m2km = 1/km2m; % km/m
+delz = .1*km2m; % m
 
+z2 = h:(delz*m2km):(h + 100); % km
 
+rho0 = n_O * mass_O; % kg/m^3
+rho = rho0;
 
+for i = 2:length(z2)
+    zi = z2(i);
 
+    r = Re + zi; % km
+    g = G*mass_earth/(r*km2m)^2; % m/s^2
+    H = kb*T_O/mass_O/g;
+    rho(i) = rho(i - 1)*exp(-delz/H); % kg/m^3
+end
+N = rho/mass_O; % number of ions / m^3
 
+% ideal gas law
+p = N*kb*T_O; % Pa
+
+% forward difference
+dpdz = diff(p(1:2)) / diff(z2(1:2)); % Pa/km
+
+figure
+plot(p, z2)
+grid on
+xlabel('Pressure (Pa)')
+ylabel('alt (km)')
+
+fprintf('4: %e, Pa/km\n', dpdz)
 
 
 
